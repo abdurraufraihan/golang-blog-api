2fea27378d53099e2ac4b59798bf2c8e41021cde
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2022-03-03 07:55:17 +0600

    refactor db-config to get db configuration from another function

--- config/database-config.go
+++ config/database-config.go
@@ -1,13 +1,47 @@
 package config
 
 import (
+	"fmt"
+
 	"github.com/abdurraufraihan/golang-blog-api/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
+type dbConfig struct {
+	host     string
+	user     string
+	password string
+	dbname   string
+	port     int
+	sslmode  string
+	timezone string
+}
+
+func getDBConfig() *dbConfig {
+	return &dbConfig{
+		host:     "localhost",
+		user:     "postgres",
+		password: "123456",
+		dbname:   "goblog",
+		port:     5432,
+		sslmode:  "disable",
+		timezone: "UTC",
+	}
+}
+
 func ConnectWithDB() *gorm.DB {
-	dns := "host=localhost user=postgres password=123456 dbname=goblog port=5432 sslmode=disable TimeZone=UTC"
+	dbConfig := getDBConfig()
+	dns := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=%s TimeZone=%s",
+		dbConfig.host,
+		dbConfig.user,
+		dbConfig.password,
+		dbConfig.dbname,
+		dbConfig.port,
+		dbConfig.sslmode,
+		dbConfig.timezone,
+	)
 	db, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
 	if err != nil {
 		panic("Failed to create connection with database")
