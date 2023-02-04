ff34610a5b8efcd4210e95e9c034957737d7a172
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2023-02-04 17:03:39 +0600

    APP_PORT & JWT_SECRET_KEY read inside code

--- cmd/server/mian.go
+++ cmd/server/mian.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/abdurraufraihan/golang-blog-api/adapter"
 	"github.com/abdurraufraihan/golang-blog-api/internal/route"
 	"github.com/gin-gonic/gin"
@@ -13,5 +15,5 @@ func main() {
 	defer adapter.CloseDbConnection(db)
 	router := gin.Default()
 	route.RootRoute(db, router)
-	router.Run(":8000")
+	router.Run(":" + os.Getenv("APP_PORT"))
 }
--- internal/service/jwt-service.go
+++ internal/service/jwt-service.go
@@ -28,9 +28,9 @@ func NewJwtService() *jwtService {
 }
 
 func getSecretKey() string {
-	secretKey := os.Getenv("JWT_SECRET")
+	secretKey := os.Getenv("JWT_SECRET_KEY")
 	if secretKey != "" {
-		secretKey = "d47s4654dsf545sdf54sdf8"
+		secretKey = "fc93cb07e1ad92898527100e58a1cf1d1e7"
 	}
 	return secretKey
 }
@@ -67,7 +67,7 @@ func (service *jwtService) GenerateTokenPair(userId interface{}) map[string]stri
 func (service *jwtService) ValidateToken(tokenString string) (*jwt.Token, error) {
 	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 		return []byte(service.secretKey), nil
 	})
