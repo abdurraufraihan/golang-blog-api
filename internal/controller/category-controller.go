6a1b0b409e3c8a04d1af0540a50bcc65a700e658
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2023-02-05 23:22:54 +0600

    category doc files generated

--- internal/controller/category-controller.go
+++ internal/controller/category-controller.go
@@ -27,12 +27,27 @@ func NewCategoryController(
 	return &categoryController{categoryService: categoryService}
 }
 
+// GetCategories             godoc
+// @Summary      Get categories list
+// @Description  Responds with the list of all categories as JSON.
+// @Tags         categories
+// @Produce      json
+// @Success      200  {object}  serializer.CategoryResponse
+// @Router       /categories [get]
 func (controller *categoryController) All(context *gin.Context) {
 	categories := controller.categoryService.All()
 	serializer := serializer.CategoriesSerializer{Categories: categories}
 	context.JSON(http.StatusOK, serializer.Response())
 }
 
+// InsertCategory             godoc
+// @Summary      Insert category
+// @Description  Responds with category as JSON.
+// @Tags         categories
+// @Produce      json
+// @Param data body dto.Category true "Category dto"
+// @Success      201  {object}  serializer.CategoryResponse
+// @Router       /categories [post]
 func (controller *categoryController) Insert(context *gin.Context) {
 	categoryDto := dto.Category{}
 	err := context.ShouldBindJSON(&categoryDto)
@@ -45,6 +60,15 @@ func (controller *categoryController) Insert(context *gin.Context) {
 	context.JSON(http.StatusOK, serializer.Response())
 }
 
+// UpdateCategory             godoc
+// @Summary      Update category
+// @Description  Responds with category as JSON.
+// @Tags         categories
+// @Produce      json
+// @Param        id  path      uint  true  "update category by id"
+// @Param data body dto.Category true "Category dto"
+// @Success      200  {object}  serializer.CategoryResponse
+// @Router       /categories/{id} [put]
 func (controller *categoryController) Update(context *gin.Context) {
 	categoryDto := dto.Category{}
 	err := context.ShouldBindJSON(&categoryDto)
@@ -62,6 +86,14 @@ func (controller *categoryController) Update(context *gin.Context) {
 	context.JSON(http.StatusOK, serializer.Response())
 }
 
+// DeleteCategory             godoc
+// @Summary      Delete category
+// @Description  Responds with category as JSON.
+// @Tags         categories
+// @Produce      json
+// @Param        id  path      uint  true  "delete category by id"
+// @Success      204
+// @Router       /categories/{id} [delete]
 func (controller *categoryController) DeleteById(context *gin.Context) {
 	categoryId, _ := strconv.ParseUint(context.Param("categoryId"), 10, 64)
 	result := controller.categoryService.DeleteById(categoryId)
