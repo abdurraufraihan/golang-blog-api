b340426b4da1383fac1ba9c793f0bddc4a4b4ed8
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2023-02-15 00:20:13 +0600

    added comments to PostResponse serializer

--- internal/serializer/post-serializer.go
+++ internal/serializer/post-serializer.go
@@ -7,13 +7,14 @@ import (
 )
 
 type PostResponse struct {
-	ID          uint             `json:"id"`
-	Title       string           `json:"title"`
-	Description string           `json:"description"`
-	Image       string           `json:"image"`
-	CreatedAt   time.Time        `json:"created_at"`
-	UpdatedAt   time.Time        `json:"updated_at"`
-	Category    CategoryResponse `json:"category"`
+	ID          uint              `json:"id"`
+	Title       string            `json:"title"`
+	Description string            `json:"description"`
+	Image       string            `json:"image"`
+	CreatedAt   time.Time         `json:"created_at"`
+	UpdatedAt   time.Time         `json:"updated_at"`
+	Category    CategoryResponse  `json:"category"`
+	Comments    []CommentResponse `json:"comments"`
 }
 
 type PostSerializer struct {
@@ -22,6 +23,7 @@ type PostSerializer struct {
 
 func (serializer *PostSerializer) Response() PostResponse {
 	categorySerializer := CategorySerializer{Category: serializer.Post.Category}
+	commentSerializer := CommentsSerializer{Comments: serializer.Post.Comments}
 	return PostResponse{
 		ID:          serializer.Post.ID,
 		Title:       serializer.Post.Title,
@@ -30,6 +32,7 @@ func (serializer *PostSerializer) Response() PostResponse {
 		CreatedAt:   serializer.Post.CreatedAt,
 		UpdatedAt:   serializer.Post.UpdatedAt,
 		Category:    categorySerializer.Response(),
+		Comments:    commentSerializer.Response(),
 	}
 }
 
