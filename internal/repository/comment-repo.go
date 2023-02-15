7b1229b7849e1c4aa50fb012ce8b6818ff13219f
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2023-02-15 08:56:15 +0600

    AllCommentByPostId added comment repository

--- internal/repository/comment-repo.go
+++ internal/repository/comment-repo.go
@@ -1,11 +1,14 @@
 package repository
 
 import (
+	"strconv"
+
 	"github.com/abdurraufraihan/golang-blog-api/internal/model"
 	"gorm.io/gorm"
 )
 
 type CommentRepo interface {
+	AllCommentByPostId(limit string, offset string, postId uint) []model.Comment
 	Insert(comment model.Comment) model.Comment
 }
 
@@ -15,6 +18,23 @@ type commentRepo struct {
 
 func NewCommentRepo(db *gorm.DB) *commentRepo {
 	return &commentRepo{db: db}
+}
+
+func (repo *commentRepo) AllCommentByPostId(
+	limit string, offset string, postId uint,
+) []model.Comment {
+	commentLimit, err := strconv.Atoi(limit)
+	if err != nil || commentLimit == 0 {
+		commentLimit = -1
+	}
+	commentOffset, err := strconv.Atoi(offset)
+	if err != nil || commentOffset == 0 {
+		commentOffset = -1
+	}
+	var comments []model.Comment
+	repo.db.Where("post_id = ?", postId).Limit(commentLimit).
+		Offset(commentOffset).Find(&comments)
+	return comments
 }
 
 func (repo *commentRepo) Insert(comment model.Comment) model.Comment {
