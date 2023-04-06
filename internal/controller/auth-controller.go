1aab9f704e13f655a9adffd98d6302e56ad84208
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2023-04-06 06:27:49 +0600

    response utils added to auth controller

--- internal/controller/auth-controller.go
+++ internal/controller/auth-controller.go
@@ -40,16 +40,16 @@ func (controller *authController) Login(context *gin.Context) {
 	var loginDto dto.Login
 	err := context.ShouldBindJSON(&loginDto)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.JSON(http.StatusBadRequest, utils.GetErrorResponse(err.Error()))
 		return
 	}
 	isValidCredential, userId := controller.authService.VerifyCredential(loginDto.Email, loginDto.Password)
 	if isValidCredential {
 		tokenPair := controller.jwtService.GenerateTokenPair(userId)
-		context.JSON(http.StatusOK, tokenPair)
+		context.JSON(http.StatusOK, utils.GetResponse(tokenPair))
 		return
 	}
-	context.JSON(http.StatusBadRequest, gin.H{"error": "invalid credential"})
+	context.JSON(http.StatusBadRequest, utils.GetErrorResponse("invalid credential"))
 }
 
 // Register             godoc
@@ -64,15 +64,15 @@ func (controller *authController) Register(context *gin.Context) {
 	var userDto dto.User
 	err := context.ShouldBindJSON(&userDto)
 	if err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.JSON(http.StatusBadRequest, utils.GetErrorResponse(err.Error()))
 		return
 	}
 	result, user := controller.authService.Register(userDto)
 	if result.Error != nil {
-		context.JSON(http.StatusBadRequest, result.Error)
+		context.JSON(http.StatusBadRequest, utils.GetErrorResponse(result.Error.Error()))
 		return
 	}
-	context.JSON(http.StatusOK, user)
+	context.JSON(http.StatusOK, utils.GetResponse(user))
 }
 
 // Verify Token             godoc
@@ -86,15 +86,15 @@ func (controller *authController) Register(context *gin.Context) {
 func (controller *authController) VerifyToken(context *gin.Context) {
 	tokenDto := dto.Token{}
 	if err := context.ShouldBindJSON(&tokenDto); err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.JSON(http.StatusBadRequest, utils.GetErrorResponse(err.Error()))
 		return
 	}
 	token, _ := utils.ValidateToken(tokenDto.Token)
 	if token == nil || !token.Valid {
-		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"is_valid": false})
+		context.AbortWithStatusJSON(http.StatusBadRequest, utils.GetErrorResponse("Invalid Token"))
 		return
 	}
-	context.JSON(http.StatusOK, gin.H{"is_valid": true})
+	context.JSON(http.StatusOK, utils.GetResponse(gin.H{"is_valid": true}))
 }
 
 // Refresh Token             godoc
@@ -108,17 +108,17 @@ func (controller *authController) VerifyToken(context *gin.Context) {
 func (controller *authController) RefreshToken(context *gin.Context) {
 	tokenDto := dto.Token{}
 	if err := context.ShouldBindJSON(&tokenDto); err != nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.JSON(http.StatusBadRequest, utils.GetErrorResponse(err.Error()))
 		return
 	}
 	token, err := utils.ValidateToken(tokenDto.Token)
 	if token == nil || !token.Valid {
-		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		context.AbortWithStatusJSON(http.StatusBadRequest, utils.GetErrorResponse(err.Error()))
 		return
 	}
 	if claims, ok := token.Claims.(jwt.MapClaims); ok {
 		context.JSON(http.StatusOK, controller.jwtService.GenerateTokenPair(claims["user_id"]))
 	} else {
-		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "can not able to claim"})
+		context.AbortWithStatusJSON(http.StatusBadRequest, utils.GetErrorResponse("Can not able to claim token"))
 	}
 }
